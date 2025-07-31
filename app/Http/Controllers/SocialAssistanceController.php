7b6d5d0cfce5b61fc8d5f116d96bbe23a9f56008
Dian Susanto <diansusanto92@gmail.com>
2025-07-31 10:35:14 +0700

    Membuat GetAll & paginasi Social Assistance

---
+++ app/Http/Controllers/SocialAssistanceController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseHelper;
+use App\Http\Resources\PaginateResource;
+use App\Http\Resources\SocialAssistanceResource;
+use App\Interfaces\SocialAssistanceRepositoryInterface;
+use Illuminate\Http\Request;
+
+class SocialAssistanceController extends Controller
+{
+
+    private SocialAssistanceRepositoryInterface $socialAssistanceRepository;
+
+    public function __construct(SocialAssistanceRepositoryInterface $socialAssistanceRepository)
+    {
+        $this->socialAssistanceRepository = $socialAssistanceRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        try {
+            $socialAssistances = $this->socialAssistanceRepository->getAll(
+                $request->search,
+                $request->limit,
+                true
+            );
+
+            return ResponseHelper::JsonResponse(true, 'Data Bantuan Sosial Berhasil Diambil', SocialAssistanceResource::collection($socialAssistances), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::JsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    public function getAllPaginated(Request $request)
+    {
+        $request = $request->validate([
+            'search' => 'nullable|string',
+            'row_per_page' => 'required|integer'
+        ]);
+
+        try {
+            $socialAssistances = $this->socialAssistanceRepository->getAllPaginated(
+                $request['search'] ?? null,
+                $request['row_per_page']
+            );
+
+            return ResponseHelper::JsonResponse(true, 'Data Bantuan Sosial Berhasil Diambil', PaginateResource::make($socialAssistances, SocialAssistanceResource::class), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::JsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
