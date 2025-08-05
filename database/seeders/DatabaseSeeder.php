1799071c1391079954333d7726b4f39253d85b35
Dian Susanto <diansusanto92@gmail.com>
2025-08-05 17:35:52 +0700

    crud development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             SocialAssistanceRecipientSeeder::class,
             EventSeeder::class,
             EventParticipantSeeder::class,
+            DevelopmentSeeder::class,
         ]);
     }
 }
