6e83242574f7de53ded601f201332ede28f9a2b9
Dian Susanto <diansusanto92@gmail.com>
2025-08-07 10:50:12 +0700

    ganti AuthRepository

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
-    Spatie\permission\PermissionServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
